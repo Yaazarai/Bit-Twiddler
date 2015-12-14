22720a0ca82a6a7ab680692ac58c520e76d61929
FatalSleep <fatalsleep@gmail.com>
2015-12-13 20:17:17 -0700

    Made spacing consistent.

--- TwiddleBIT.cs
+++ TwiddleBIT.cs
@@ -1,20 +1,20 @@
 // Provides methods for twiddling multiple bits at once.
 public static class BitTwiddle {
-    private static int BITSIZE_UINT = (sizeof(uint) * 8);
+    private static int BITSIZE_UINT = sizeof(uint) * 8;
 
     // Zeros a total of "length" bits from the start "index."
     public static uint ZeroBits( uint value, int length, int index ) {
-        return value & ~((uint.MaxValue >> (BITSIZE_UINT - length)) << index);
+        return value & ~( ( uint.MaxValue >> ( BITSIZE_UINT - length ) ) << index );
     }
 
     // Sets a total of "length" bits from the start "index," to 1.
     public static uint SetBits( uint value, int length, int index ) {
-        return value | ((uint.MaxValue >> (BITSIZE_UINT - length)) << index);
+        return value | ( ( uint.MaxValue >> ( BITSIZE_UINT - length ) ) << index );
     }
 
     // Toggles On/Off a total of "length" bits from the start "index."
     public static uint FlipBits( uint value, int length, int index ) {
-        return value ^ ((uint.MaxValue >> (BITSIZE_UINT - length)) << index);
+        return value ^ ( ( uint.MaxValue >> ( BITSIZE_UINT - length ) ) << index );
     }
 
     // Fills a total of "length" bits with the new set of "bits" from the start "index."
